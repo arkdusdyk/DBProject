41c16a5f418114957899c430290b4109c2c0fd6a
arkdusdyk <69205130+arkdusdyk@users.noreply.github.com>
2020-09-12 01:34:22 +0900

    Update KnnSearch.py

--- KnnSearch.py
+++ KnnSearch.py
@@ -10,11 +10,13 @@ class knnsearch:
     def knn(self, data, currentlat, currentlon, k):
         knnlist = []
         knn_id_list = list(self.index.nearest((currentlat, currentlon, currentlat, currentlon),k))#return only id
+        knn_id_list = map(int,knn_id_list)
+        #print(knn_id_list)
         data_dic = dict()
         #for searching by id, make dictionary
         for items in data:
             idfordata = '%d'%items[0]
-            data_dic[idfordata]=data[1:4]
+            data_dic[idfordata]=items[1:4]
         for idx in knn_id_list:
             idx_str = '%d'%idx
             tmp = []
@@ -22,10 +24,12 @@ class knnsearch:
             tmp.extend(data_dic[idx_str])
             #tmp = [id, review, lat, lon] by knn
             knnlist.append(tmp)
+        #print(knnlist)
         return knnlist
 
     def rtree(self, data):
         for ones in data:
+            #print(ones[0])
             if (ones[2] == None or ones[3] == None):
                 continue
             self.index.insert(ones[0],(ones[2],ones[3],ones[2],ones[3]))
